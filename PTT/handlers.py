588ecb1b7bdda91e9fedb5783d3f80c23eb0c453
davidemarcoli <davide@marcoli.ch>
2024-04-29 12:05:03 +0200

    Add handler to remove spaces in codec names

--- PTT/handlers.py
+++ PTT/handlers.py
@@ -118,6 +118,10 @@ def add_defaults(parser: Parser):
     parser.add_handler("codec", regex.compile(r"\b[xh][-. ]?26[45]", regex.IGNORECASE), lowercase, {"remove": True})
     parser.add_handler("codec", regex.compile(r"\bhevc(?:\s?10)?\b", regex.IGNORECASE), value("hevc"), {"remove": True, "skipIfAlreadyFound": False})
     parser.add_handler("codec", regex.compile(r"\b(?:dvix|mpeg2|divx|xvid|avc)\b", regex.IGNORECASE), lowercase, {"remove": True, "skipIfAlreadyFound": False})
+    def handle_space_in_codec(context):
+        if context["result"].get("codec"):
+            context["result"]["codec"] = regex.sub("[ .-]", "", context["result"]["codec"])
+    parser.add_handler("codec", handle_space_in_codec)
 
     # Audio
     parser.add_handler("audio", regex.compile(r"7\.1[. ]?Atmos\b", regex.IGNORECASE), value("7.1 Atmos"), {"remove": True})
