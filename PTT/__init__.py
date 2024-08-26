a9da5866b234a8d956f6fecce9703e5902912930
mhdzumair <mhdzumair@gmail.com>
2024-08-26 22:21:08 +0530

    update parse_title function

--- PTT/__init__.py
+++ PTT/__init__.py
@@ -5,13 +5,15 @@ _parser = Parser()
 add_defaults(_parser)
 
 
-def parse_title(raw_title: str) -> dict:
+def parse_title(raw_title: str, translate_languages: bool = False) -> dict:
     """
     Parse the given input string using the initialized parser instance.
+
     :param raw_title: The input raw torrent title to parse.
+    :param translate_languages: Whether to translate language codes to language names or short codes (default: False returns short codes)
     :return: A dictionary with the parsed results.
     """
-    return _parser.parse(raw_title)
+    return _parser.parse(raw_title, translate_languages)
 
 
 __all__ = ["Parser", "add_defaults", "parse", "parse_title", "handlers", "transformers"]
